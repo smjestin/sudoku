be7878223daf9342c0a1e72a12a2f28d1a11259c
Shelby Jestin <shelbyjestin@macbook-air-3.local>
2016-01-15 10:16:05 -0400

    Initial Commit

---
+++ Sudoku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sudoku
+//
+//  Created by Shelby Jestin on 2016-01-15.
+//  Copyright © 2016 Shelby Jestin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
